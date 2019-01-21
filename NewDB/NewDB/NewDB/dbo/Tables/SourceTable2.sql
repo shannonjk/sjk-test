b76fe3876e63b0c28492bf7d997d201b12580d4b
Shannon Bananen <shannonjk@gmail.com>
2019-01-21 14:30:51 -0800

    testing updates from vscode

--- NewDB/NewDB/NewDB/dbo/Tables/SourceTable2.sql
+++ NewDB/NewDB/NewDB/dbo/Tables/SourceTable2.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[SourceTable2] (
     [NaturalKey] INT           IDENTITY (1, 1) NOT NULL,
     [COL2]       VARCHAR (128) NULL,
+    COL3 VARCHAR(1) NULL
     PRIMARY KEY CLUSTERED ([NaturalKey] ASC)
 );
 
